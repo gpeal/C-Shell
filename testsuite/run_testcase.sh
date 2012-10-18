8a7be9a1d45cf8c6c4d116bd55d715af4d213ba4
Gabriel Peal <gabriel@gpeal.com>
2012-10-18 15:17:21 -0500

    Replaced testsuite with part 2

--- testsuite/run_testcase.sh
+++ testsuite/run_testcase.sh
@@ -1,11 +1,10 @@
-#!/bin/bash
-# On Ubuntu, /bin/sh is dash. dash can't run this file.
+#!/bin/sh
 
 source ./config.test;
 
 if [[ ! "$#" -eq 1 ]]; then
-  echo -e "usage: $0 targz_file";
-  exit 1;
+	echo -e "usage: $0 targz_file";
+	exit 1;
 fi;
 
 TARGZ=$1;
@@ -18,11 +17,8 @@ chmod go-rwx ${SRCDIR} ${TMP} ${OUTPUT} || exit 1;
 
 function cleanUp()
 {
-  rm -Rf ${SRCDIR} ${TMP} ${OUTPUT};
-  sh ${TC_DIR}/remove_extra.sh;
+	rm -Rf ${SRCDIR} ${TMP} ${OUTPUT};
 }
-
-trap 'cleanUp; exit 1' 2
 
 echo "Testing ${TARGZ}";
 echo;
@@ -36,14 +32,14 @@ echo;
 
 if [[ `ls *.c |wc -w` -eq 0 ]]; then
     echo "error: no source files (*.c) found";
-  echo "Please follow the submission instructions carefully.";
-  cleanUp;
+	echo "Please follow the submission instructions carefully.";
+	cleanUp;
     exit 1;
 fi;
 
 if [ ! -f Makefile -a ! -f makefile ]; then
-  echo "warning: Makefile is missing";
-  echo;
+	echo "warning: Makefile is missing";
+	echo;
 fi;
 
 # Setup the environment 
@@ -56,12 +52,12 @@ gcc ${TC_DIR}/myspin.c -o myspin
 # Compile the code
 echo "COMPILE"
 for SRC in ${SRCDIR}/*.c; do
-  echo "compile $SRC";
-  gcc -g -Wall -O2 -D HAVE_CONFIG_H -c ${SRC} >> ${OUTPUT}/gcc.output 2>&1;
-  echo "-----" >> ${OUTPUT}/gcc.output;
+	echo "compile $SRC";
+	gcc -g -Wall -O2 -D HAVE_CONFIG_H -c ${SRC} >> ${OUTPUT}/gcc.output 2>&1;
+	echo "-----" >> ${OUTPUT}/gcc.output;
 done
 echo "LINKING";
-gcc *.o -o ${TMP}/${BIN} >> ${OUTPUT}/gcc.output 2>&1;
+gcc	*.o -o ${TMP}/${BIN} >> ${OUTPUT}/gcc.output 2>&1;
 WARNING=`grep -c warning ${OUTPUT}/gcc.output`
 ERROR=`grep -c error ${OUTPUT}/gcc.output`
 
@@ -69,16 +65,16 @@ echo "${WARNING} warning(s) found while compiling"
 echo "${ERROR} error(s) found while compiling"
 
 if [ ${WARNING} -gt 0 -o ${ERROR} -gt 0 ]; then
-  echo;
-  echo "GCC OUTPUT";
-  cat ${OUTPUT}/gcc.output;
+	echo;
+	echo "GCC OUTPUT";
+	cat ${OUTPUT}/gcc.output;
 fi;
 
 if [ ! -x ${TMP}/${BIN} ]; then
-  echo "error: failed to create executable";
-  cat ${OUTPUT}/gcc.output;
-  cleanUp;
-  exit 1;
+	echo "error: failed to create executable";
+	cat ${OUTPUT}/gcc.output;
+	cleanUp;
+	exit 1;
 fi;
 
 echo;
@@ -87,45 +83,45 @@ echo;
 echo "RUN BASIC TEST CASES";
 TC_PASSED=0;
 for tc in ${BASIC_TESTS}; do
-  ARGS="";
-  if [[ -f ${TC_DIR}/${tc}.arg ]]; then
-    ARGS="${ARGS} $(<${TC_DIR}/${tc}.arg)";
-  fi
-  if [[ -f ${TC_DIR}/${tc}.out ]]; then
-    cp ${TC_DIR}/${tc}.out ${OUTPUT}/${tc}.orig;
-  else
-    if [[ -f ${TC_DIR}/${tc}.in ]]; then
-      ./${SDRIVER} -t ${TC_DIR}/${tc}.in -s ./${ORIG} > ${OUTPUT}/${tc}.orig 2>&1;
+	ARGS="";
+	if [[ -f ${TC_DIR}/${tc}.arg ]]; then
+		ARGS="${ARGS} $(<${TC_DIR}/${tc}.arg)";
+	fi
+	if [[ -f ${TC_DIR}/${tc}.out ]]; then
+		cp ${TC_DIR}/${tc}.out ${OUTPUT}/${tc}.orig;
+	else
+		if [[ -f ${TC_DIR}/${tc}.in ]]; then
+			./${SDRIVER} -t ${TC_DIR}/${tc}.in -s ./${ORIG} > ${OUTPUT}/${tc}.orig 2>&1;
 #                        cp ${OUTPUT}/${tc}.orig ${TC_DIR}/${tc}.out;
-    else
-      ${TC_DIR}/${ORIG} ${ARGS} > ${OUTPUT}/${tc}.orig 2>&1;    
-    fi
-  fi
+		else
+			${TC_DIR}/${ORIG} ${ARGS} > ${OUTPUT}/${tc}.orig 2>&1;		
+		fi
+	fi
 
-  if [[ -f ${TC_DIR}/${tc}.in ]]; then
-    ./${SDRIVER} -t ${TC_DIR}/${tc}.in -s ./${BIN} > ${OUTPUT}/${tc}.bin 2>&1;
-  else
-    ./${BIN} ${ARGS} > ${OUTPUT}/${tc}.bin 2>&1;
-  fi
+	if [[ -f ${TC_DIR}/${tc}.in ]]; then
+		./${SDRIVER} -t ${TC_DIR}/${tc}.in -s ./${BIN} > ${OUTPUT}/${tc}.bin 2>&1;
+	else
+		./${BIN} ${ARGS} > ${OUTPUT}/${tc}.bin 2>&1;
+	fi
 
-  ${DIFF} ${OUTPUT}/${tc}.orig ${OUTPUT}/${tc}.bin >/dev/null;
-  if [[ "$?" -eq 0 ]]; then
-    echo "${tc}: PASS";
-    ((TC_PASSED++));
-  else
-    echo "${tc}: FAILED";
-    if [[ -f ${TC_DIR}/${tc}.arg ]]; then
-      echo "Args:";
-      cat ${TC_DIR}/${tc}.arg;
-    fi
-    if [[ -f ${TC_DIR}/${tc}.in ]]; then
-      echo "Input:";
-      cat ${TC_DIR}/${tc}.in;
-    fi
-    echo "-- HOW IT SHOULD BE ------------------------------------------------------------ YOUR PROGRAM --------------------------------------------------------------";
-    diff --side-by-side -W 160 ${OUTPUT}/${tc}.orig ${OUTPUT}/${tc}.bin;
-    echo "------------------------------------------------------------------------------------------------------------------------------------------------------------";
-  fi
+	${DIFF} ${OUTPUT}/${tc}.orig ${OUTPUT}/${tc}.bin >/dev/null;
+	if [[ "$?" -eq 0 ]]; then
+		echo "${tc}: PASS";
+		((TC_PASSED++));
+	else
+		echo "${tc}: FAILED";
+		if [[ -f ${TC_DIR}/${tc}.arg ]]; then
+			echo "Args:";
+			cat ${TC_DIR}/${tc}.arg;
+		fi
+		if [[ -f ${TC_DIR}/${tc}.in ]]; then
+			echo "Input:";
+			cat ${TC_DIR}/${tc}.in;
+		fi
+		echo "-- HOW IT SHOULD BE ------------------------------------------------------------ YOUR PROGRAM --------------------------------------------------------------";
+		diff --side-by-side -W 160 ${OUTPUT}/${tc}.orig ${OUTPUT}/${tc}.bin;
+		echo "------------------------------------------------------------------------------------------------------------------------------------------------------------";
+	fi
 done;
 
 echo;
@@ -133,50 +129,50 @@ echo "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=";
 echo "${TC_PASSED} basic test cases passed";
 echo;
 
-sh ${TC_DIR}/setup_extra.sh;
 # Run extra credit test cases
 echo "RUN EXTRA CREDIT TEST CASES";
 ETC_PASSED=0;
+
 for tc in ${EXTRA_TESTS}; do
-  ARGS="";
-  if [[ -f ${TC_DIR}/${tc}.arg ]]; then
-    ARGS="${ARGS} $(<${TC_DIR}/${tc}.arg)";
-  fi
-  if [[ -f ${TC_DIR}/${tc}.out ]]; then
-    cp ${TC_DIR}/${tc}.out ${OUTPUT}/${tc}.orig;
-  else
-    if [[ -f ${TC_DIR}/${tc}.in ]]; then
-                        ./${SDRIVER} -t ${TC_DIR}/${tc}.in -s ${TC_DIR}/${ORIG} > ${OUTPUT}/${tc}.orig 2>&1;
+	ARGS="";
+	if [[ -f ${TC_DIR}/${tc}.arg ]]; then
+		ARGS="${ARGS} $(<${TC_DIR}/${tc}.arg)";
+	fi
+	if [[ -f ${TC_DIR}/${tc}.out ]]; then
+		cp ${TC_DIR}/${tc}.out ${OUTPUT}/${tc}.orig;
+	else
+		if [[ -f ${TC_DIR}/${tc}.in ]]; then
+                        ./${SDRIVER} -t ${TC_DIR}/${tc}.in -s ${BASH} > ${OUTPUT}/${tc}.orig 2>&1;
 #                        cp ${OUTPUT}/${tc}.orig ${TC_DIR}/${tc}.out;
-    else
-      ${TC_DIR}/${ORIG} ${ARGS} > ${OUTPUT}/${tc}.orig 2>&1;    
-    fi
-  fi
+		else
+			${BASH} ${ARGS} > ${OUTPUT}/${tc}.orig 2>&1;		
+		fi
+	fi
 
-  if [[ -f ${TC_DIR}/${tc}.in ]]; then
+	if [[ -f ${TC_DIR}/${tc}.in ]]; then
                 ./${SDRIVER} -t ${TC_DIR}/${tc}.in -s ./${BIN} > ${OUTPUT}/${tc}.bin 2>&1;
-  else
-    ./${BIN} ${ARGS} > ${OUTPUT}/${tc}.bin 2>&1;
-  fi
+	else
+		./${BIN} ${ARGS} > ${OUTPUT}/${tc}.bin 2>&1;
+	fi
 
-  ${DIFF} ${OUTPUT}/${tc}.orig ${OUTPUT}/${tc}.bin >/dev/null;
-  if [[ "$?" -eq 0 ]]; then
-    echo "${tc}: PASS";
-    ((ETC_PASSED++));
-  else
-    echo "${tc}: FAILED";
-    if [[ -f ${TC_DIR}/${tc}.arg ]]; then
-      echo "Args:";
-      cat ${TC_DIR}/${tc}.arg;
-    fi
-    if [[ -f ${TC_DIR}/${tc}.in ]]; then
-      echo "Input:";
-      cat ${TC_DIR}/${tc}.in;
-    fi
-    echo "-- HOW IT SHOULD BE ------------------------------------------------------------ YOUR PROGRAM --------------------------------------------------------------";
-    diff --side-by-side -W 160 ${OUTPUT}/${tc}.orig ${OUTPUT}/${tc}.bin;
-    echo "------------------------------------------------------------------------------------------------------------------------------------------------------------";
-  fi
+	${DIFF} ${OUTPUT}/${tc}.orig ${OUTPUT}/${tc}.bin >/dev/null;
+	if [[ "$?" -eq 0 ]]; then
+		echo "${tc}: PASS";
+		((ETC_PASSED++));
+	else
+		echo "${tc}: FAILED";
+		if [[ -f ${TC_DIR}/${tc}.arg ]]; then
+			echo "Args:";
+			cat ${TC_DIR}/${tc}.arg;
+		fi
+		if [[ -f ${TC_DIR}/${tc}.in ]]; then
+			echo "Input:";
+			cat ${TC_DIR}/${tc}.in;
+		fi
+		echo "-- HOW IT SHOULD BE ------------------------------------------------------------ YOUR PROGRAM --------------------------------------------------------------";
+		diff --side-by-side -W 160 ${OUTPUT}/${tc}.orig ${OUTPUT}/${tc}.bin;
+		echo "------------------------------------------------------------------------------------------------------------------------------------------------------------";
+	fi
 done;
 
 echo;
@@ -187,52 +183,52 @@ echo;
 # Check for memory leaks
 echo "CHECK FOR MEMORY LEAKS";
 if [[ `which valgrind 2>&1 | grep -c "no valgrind"` -gt 0 ]]; then
-  echo "error: valgrind not in PATH";
-  echo "error: skip memory leak check";
+	echo "error: valgrind not in PATH";
+	echo "error: skip memory leak check";
 else
-  POS_LEAKS=0;
-  DEF_LEAKS=0;
-  STILL_REACHABLE=0;
+	POS_LEAKS=0;
+	DEF_LEAKS=0;
+	STILL_REACHABLE=0;
 
-  for tc in ${MEMORY_TESTS}; do
-    ARGS="";
-      if [[ -f ${TC_DIR}/${tc}.arg ]]; then
-          ARGS="${ARGS} $(<${TC_DIR}/${tc}.arg)";
-    fi
+	for tc in ${MEMORY_TESTS}; do
+		ARGS="";
+	    if [[ -f ${TC_DIR}/${tc}.arg ]]; then
+    	    ARGS="${ARGS} $(<${TC_DIR}/${tc}.arg)";
+		fi
 
-    if [[ -f ${TC_DIR}/${tc}.in ]]; then
-      valgrind -v --tool=memcheck --show-reachable=yes --leak-check=yes ./${BIN} < ${TC_DIR}/${tc}.in > ${OUTPUT}/${tc}.valgrind 2>&1;
-    else
-      valgrind -v --tool=memcheck --show-reachable=yes --leak-check=yes ./${BIN} ${ARGS} > ${OUTPUT}/${tc}.valgrind 2>&1;
-      fi
-    
-    if [[ -n ${VERBOSE} ]]; then
-      cat ${OUTPUT}/${tc}.valgrind;
-    fi;
+		if [[ -f ${TC_DIR}/${tc}.in ]]; then
+			valgrind -v --tool=memcheck --show-reachable=yes --leak-check=yes ./${BIN} < ${TC_DIR}/${tc}.in > ${OUTPUT}/${tc}.valgrind 2>&1;
+		else
+			valgrind -v --tool=memcheck --show-reachable=yes --leak-check=yes ./${BIN} ${ARGS} > ${OUTPUT}/${tc}.valgrind 2>&1;
+	    fi
+		
+		if [[ -n ${VERBOSE} ]]; then
+			cat ${OUTPUT}/${tc}.valgrind;
+		fi;
 
-    NOCHECK=`grep -c "no leaks are possible" ${OUTPUT}/${tc}.valgrind`;
-    if [[ ${NOCHECK} -eq 0 ]]; then
-      leaks=`grep "possibly lost:" ${OUTPUT}/${tc}.valgrind | sed "s/.*bytes in[[:space:]]*//" |sed "s/[[:space:]]*blocks.*//";`
-      if [[ ${leaks} -gt ${POS_LEAKS} ]]; then
-        POS_LEAKS=${leaks};
-      fi;
-      leaks=`grep "definitely lost:" ${OUTPUT}/${tc}.valgrind | sed "s/.*bytes in[[:space:]]*//" |sed "s/[[:space:]]*blocks.*//";`
-      if [[ ${leaks} -gt ${DEF_LEAKS} ]]; then
-        DEF_LEAKS=${leaks};
-      fi;
-      leaks=`grep "still reachable:" ${OUTPUT}/${tc}.valgrind | sed "s/.*bytes in[[:space:]]*//" |sed "s/[[:space:]]blocks.*//";`
-      if [[ ${leaks} -gt ${DEF_LEAKS} ]]; then
-        STILL_REACHABLE=${leaks};
-      fi;
-    fi;
-  done;
+		NOCHECK=`grep -c "no leaks are possible" ${OUTPUT}/${tc}.valgrind`;
+		if [[ ${NOCHECK} -eq 0 ]]; then
+			leaks=`grep "possibly lost:" ${OUTPUT}/${tc}.valgrind | sed "s/.*bytes in[[:space:]]*//" |sed "s/[[:space:]]*blocks.*//";`
+			if [[ ${leaks} -gt ${POS_LEAKS} ]]; then
+				POS_LEAKS=${leaks};
+			fi;
+			leaks=`grep "definitely lost:" ${OUTPUT}/${tc}.valgrind | sed "s/.*bytes in[[:space:]]*//" |sed "s/[[:space:]]*blocks.*//";`
+			if [[ ${leaks} -gt ${DEF_LEAKS} ]]; then
+				DEF_LEAKS=${leaks};
+			fi;
+			leaks=`grep "still reachable:" ${OUTPUT}/${tc}.valgrind | sed "s/.*bytes in[[:space:]]*//" |sed "s/[[:space:]]blocks.*//";`
+			if [[ ${leaks} -gt ${DEF_LEAKS} ]]; then
+				STILL_REACHABLE=${leaks};
+			fi;
+		fi;
+	done;
 
-  echo "${POS_LEAKS} possible leaks";
-  echo "${DEF_LEAKS} leaks";
-  echo "${STILL_REACHABLE} still reachable";
+	echo "${POS_LEAKS} possible leaks";
+	echo "${DEF_LEAKS} leaks";
+	echo "${STILL_REACHABLE} still reachable";
 fi;
 
-  
+	
 #Clean up
 cleanUp;
 
